56ace396d3b81fb69052ee618d9ee452db0d8fef
calumny <connortstokes@gmail.com>
2017-07-21 12:46:01 -0400

    logic updates

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ var hackerPowers = ["deludeCandidate", "revealCandidateScore", "callImmediateVot
 var presidentPowers = ["jailCitizen", "investigateRussian", "callRecount"];
 var candidateScores = [1, 2, 2, 3, 3, 5];
 var minPlayerCount = 6;
-
+var maxPlayerCount = 100;
+var redTeamName = "Republican";
+var blueTeamName = "Democrat";
+var independentTeamName = "Swing Voter";
+var hackerTeamName = "Russian";
 
 function createRoom(roomName) {
   if (rooms[roomName] != undefined) {
@@ -40,6 +44,7 @@ function createRoom(roomName) {
                     players:{},
                     redTeam:[],
                     blueTeam:[],
+                    independentTeam:[],
                     hackerTeam:[],
                     redCandidate:null,
                     blueCandidate:null,
@@ -55,6 +60,58 @@ function createRoom(roomName) {
   }
 }
 
+function prepGame(roomName) {
+  var room = rooms[roomName];
+  var roomPlayers = Object.keys(room.players);
+  if (roomPlayers.length >= minPlayerCount && roomPlayers.length <= maxPlayerCount) {
+    var hacker = roomPlayers.splice(Math.floor(Math.random()*roomPlayers.length), 1);
+    room.hackerTeam = [hacker];
+
+    if (roomPlayers.length % 2 == 1) {
+      var independent = roomPlayers.splice(Math.floor(Math.random()*roomPlayers.length), 1);
+      room.independentTeam =  [independent];
+    }
+
+    var remainingPlayers = roomPlayers.length;
+
+    for (var i=0; i < remainingPlayers/2; i++) {
+      var redPlayer = roomPlayers.splice(Math.floor(Math.random()*roomPlayers.length), 1);
+      var redTeam = room.redTeam;
+      redTeam.push(redPlayer);
+      room.redTeam = redTeam;
+    }
+
+    room.blueTeam = roomPlayers;
+
+    rooms[roomName] = room;
+
+  } else {
+    console.log('not enough players to start game');
+  }
+}
+
+function startGame(roomName){
+  try {
+    room = rooms[roomName];
+    for (var i = 0; i < room.redTeam.length; i ++) {
+      io.to(room.redTeam[i]).emit('starting game', redTeamName);
+    }
+    for (var i = 0; i < room.blueTeam.length; i ++) {
+      io.to(room.blueTeam[i]).emit('starting game', blueTeamName);
+    }
+    for (var i = 0; i < room.independentTeam.length; i ++) {
+      io.to(room.independentTeam[i]).emit('starting game', independentTeamName);
+    }
+    for (var i = 0; i < room.hackerTeam.length; i ++) {
+      io.to(room.hackerTeam[i]).emit('starting game', hackerTeamName);
+    }
+    room.state = roomStates.announceCandidates;
+    rooms[roomName] = room;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 function getUniqueNickname(nicks, originalNickname, newNickname, numberAppend) {
   if (nicks.indexOf(newNickname) > -1) {
     return getUniqueNickname(nicks, originalNickname, originalNickname + numberAppend, numberAppend + 1);
@@ -64,7 +121,7 @@ function getUniqueNickname(nicks, originalNickname, newNickname, numberAppend) {
 }
 
 function addPlayerToRoom(roomName, socket) {
-  if (rooms[roomName]!=undefined) {
+  if (rooms[roomName]!=undefined && rooms[roomName].state == roomStates.lobby) {
     var room = rooms[roomName];
     var roomPlayers = room.players;
     var nicks = Object.keys(roomPlayers).map(function(key) {
@@ -80,21 +137,21 @@ function addPlayerToRoom(roomName, socket) {
 
 function joinRoom(roomName, socket) {
   socket.join(roomName);
-  // try {
+  try {
     addPlayerToRoom(roomName, socket);
     var playerNicks = Object.keys(rooms[roomName].players).map(function(key) {
         return rooms[roomName].players[key];
     });
-    socket.emit('successfully joined room', playerNicks);
+    socket.emit('successfully joined room', {roomName:roomName, players:playerNicks});
     socket.broadcast.to(roomName).emit('new player joined room', playerNicks);
     if (playerNicks.length >=  minPlayerCount) {
       io.to(roomName).emit('enough players');
+      console.log('enough players in room ' + roomName);
     }
     console.log(socket.nickname + " joined room " + roomName);
-  // } catch (err) {
-  //   socket.emit('failed to join room');
-  //   console.log(err);
-  // }
+  } catch (err) {
+    throw(err);
+  }
 }
 
 createRoom('test room');
@@ -113,18 +170,38 @@ io.on('connection', function(socket){
     }
   });
 
-  socket.on('join room', function(msg){
+  socket.on('join room', function(msg, callback){
     var roomName = msg.trim().toLowerCase();
-    if (roomName.length == 4) {
-      joinRoom(roomName, socket);
-    } else {
-      socket.emit('invalid room');
+    try {
+      if (roomName.length == 4) {
+        joinRoom(roomName, socket);
+        callback({success:true});
+      } else {
+        callback({error:'invalid room'});
+      }
+    } catch (err) {
+      callback({error:err});
     }
   });
 
-  socket.on('join public room', function(){
+  socket.on('join public room', function(callback){
     var roomName = 'test room';
-    joinRoom(roomName, socket);
+    try {
+      joinRoom(roomName, socket);
+      callback({success:true});
+    } catch(err) {
+      callback({error:err});
+    }
+  });
+
+  socket.on('start game countdown', function(msg) {
+    console.log('start game received for ' + msg);
+    if (msg in rooms && socket.id in rooms[msg].players) {
+      console.log('prep game');
+      prepGame(msg);
+      console.log('start game');
+      startGame(msg);
+    }
   });
 
   socket.on('chat message', function(msg){
